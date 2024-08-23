50813b538511ceea28b8ec4d965a2c33deea304d
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-22 20:11:30 -0600

    Implementacion de clases

---
+++ ICliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_Tienda
+{
+    internal interface ICliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Télefono { get; set; }
+    }
+}
---
+++ Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_Tienda
+{
+    internal class Venta
+    {
+        public int Id { get; set; }
+        public string NombreCliente { get; set; }
+        public string NombreProducto { get; set; }
+        public double Total { get; set; }
+    }
+}
