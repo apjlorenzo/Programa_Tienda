ce8dec3b5269d3959164ae0965b3f29c9d854360
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-22 20:09:42 -0600

    Clases

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
 ﻿
+using Programa_Tienda;
+
+Menu menu = new Menu();
+bool menuCondition = true;
+do
+{
+    menu.MenuPrincipal();
+    int opcion = int.Parse(Console.ReadLine());
+    switch(opcion)
+    {
+        case 1:
+            menu.MenuProductos();
+            break;
+        case 2:
+            menu.MenuCliente();
+            break;
+        case 3:
+            break;
+        case 4:
+            menuCondition = false;
+            break;
+    }
+}
+while (menuCondition);
